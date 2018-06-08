2ae1c20b3f388f9a0acd7e0214a25ed5ced32e3d
Margaret Dimond <margaretdimond@gmail.com>
2018-06-08 18:33:17 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,19 +1,31 @@
-board = Array.new(9, " ")
+#board = Array.new(9, " ")
+# def turn_count (board)
+#   counter = 0
+#   board.each do |position|
+#     if (position == "X") || (position == "O")
+#       counter += 1
+#    else
+#    end
+#  end
+#  counter.to_i
+# end
+
 def turn_count (board)
-  counter = 0
-  board.each do |position|
-    if (position == "X") || (position == "O")
-      counter += 1
-   else
-   end
- end
- counter.to_i
+  board.count {|token| token == "X" || token == "O"}
 end
 
+
+
+
+=begin
 def current_player (board, currect_player = "X")
-if turn_count(board).even?
-"X"
-else
-"O"
-end
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
+=end
+
+def current_player (board, currect_player = "X")
+  turn_count(board).even? ? "X" : "O"
 end
